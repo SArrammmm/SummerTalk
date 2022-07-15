aa6fdd2be951d964e4c27db4cea72433a86a7fa4
Alireza <alirezarezaei8080@gmail.com>
2022-07-15 11:40:31 +0430

    basic login procedure added

--- src/server.py
+++ src/server.py
@@ -23,7 +23,21 @@ def adduser():
         return "Username already taken!"
     hashed_pass = hashlib.sha256(payload["password"].encode("utf-8")).hexdigest()
     added_user = coll.insert_one({"username":payload["username"],"password":hashed_pass})
-    return "User created with token {}".format(added_user.inserted_id)         
+    return "User created with token {}".format(added_user.inserted_id)
+
+@app.route("/users/login",methods=["POST"])
+def login():
+    payload = request.get_json(force=True)
+    client = pymongo.MongoClient("mongodb://localhost:27017")
+    db = client["SummerTalk"]
+    coll = db["users"]
+    user = coll.find_one({"username":payload["username"]})
+    if not user:
+        return "No user found with such criteria"
+    if user["password"] == hashlib.sha256(payload["password"].encode("utf-8")).hexdigest():
+        return "Authentication successful! Token : {}".format(user["_id"])
+    else:
+        return "Authentication failed due to incorrect credentials"    
 
 if __name__ == "__main__":
     app.run(debug=True)    
