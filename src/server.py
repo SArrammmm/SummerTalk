e05d4c442075f713a809e582e55ff66491ced84e
Alireza <alirezarezaei8080@gmail.com>
2022-07-15 11:34:55 +0430

    very basic sign-up mechanism added

--- src/server.py
+++ src/server.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
-from flask import Flask , render_template
+import random
+import string
+import hashlib
+import pymongo
+
+from flask import Flask , render_template , request
 
 app = Flask(__name__)
 
@@ -8,5 +13,17 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
+@app.route("/users/add",methods=["POST"])
+def adduser():
+    payload = request.get_json(force=True)
+    client = pymongo.MongoClient("mongodb://localhost:27017")
+    db = client["SummerTalk"]
+    coll = db["users"]
+    if coll.find_one({"username":payload["username"]}):
+        return "Username already taken!"
+    hashed_pass = hashlib.sha256(payload["password"].encode("utf-8")).hexdigest()
+    added_user = coll.insert_one({"username":payload["username"],"password":hashed_pass})
+    return "User created with token {}".format(added_user.inserted_id)         
+
 if __name__ == "__main__":
-    app.run()    
+    app.run(debug=True)    
